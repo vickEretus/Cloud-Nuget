c50258cc8bae1f31d786622ef8dc7e8f1677884c
UnicycleUnicorn <brand227796@gmail.com>
2023-10-04 10:54:16 -0400

    Change connection string to a universal value

--- Database/Program.cs
+++ Database/Program.cs
@@ -1,54 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-    class Program
+
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string connectionString;
         {
-             string connectionString = "Server=DESKTOP-AFT5UDN;Database=Testing;User Id=Jordan;Password=JORDAN;";
-        // Attempting to make a connection to the mini    
-        // string connectionString = "Server=DESKTOP-IBU02IL;Database=Testing;User Id=Ycp123;Password=Ycp123;";
+            string databaseName = "revmetrix-u";
+            connectionString = $"Server=localhost;database={databaseName};Integrated Security=True;";
+        }
 
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (var connection = new SqlConnection(connectionString))
+        {
+            try
             {
-                try
-                {
-                    connection.Open();
+                connection.Open();
 
-                    int userId = 12; // Replace with the specific user ID you want to retrieve
-
-                    string query = "SELECT * FROM [User] WHERE id = @UserId";
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@UserId", userId);
-
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.Read())
-                    {
-                        Console.WriteLine($"User ID: {reader["id"]}");
-                        Console.WriteLine($"Name: {reader["name"]}");
-                        Console.WriteLine($"Email: {reader["email"]}");
-                        Console.WriteLine($"Phone: {reader["phone"]}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("User not found.");
-                    }
-
-                    reader.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error: " + ex.Message);
-                }
-                finally
-                {
-                    connection.Close();
-                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
             }
         }
     }
+}
